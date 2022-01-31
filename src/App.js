d285f655962ccfb67cb5d667f4b3729554623b11
Laguda Stefano <lagudadev@gmail.com>
2022-01-31 19:11:36 +0100

    feat: added initial files for project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Title from "./components/Title";
+import Holiday from "./components/Holiday";
+
+function App() {
+  return (
+    <section className="section-center">
+      <div>APP</div>
+    </section>
+  );
+}
+
+export default App;
